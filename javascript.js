49defa7d23083a98277bf17e820fde8a67e268ef
matucavallo <130611794+matucavallo@users.noreply.github.com>
2023-05-21 19:43:12 -0300

    Clase 10 (Javascript)

---
+++ javascript.js
@@ -0,0 +1,31 @@
+var nombre = "Mateo"; // string
+
+var apellido = "Cavallo"; // string
+
+console.log(nombre + apellido); 
+
+var edad = 18; // number
+
+var altura = 176; // number
+
+var suma = (edad + altura)
+
+console.log(suma)
+
+var sumadetodo = (suma + 500) // number
+
+console.log(sumadetodo)
+
+
+
+
+
+
+
+var nombre = "jorgito"
+var edad = "78"
+var suma = (nombre + edad)
+
+console.log(suma)
+
+// suma de un string con un number a ver que pasa
